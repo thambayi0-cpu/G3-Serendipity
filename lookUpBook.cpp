b913ca8c99dffc791dd7b1a165bf861810c02f8d
Thokozile Hambayi <thambayi0@saddleback.edu>
2025-11-15 00:21:43 -0800

    Properly implemented lookUpBook

--- lookUpBook.cpp
+++ lookUpBook.cpp
@@ -25,10 +25,11 @@ void lookUpBook (bool &keepInvMenuActive, bool &keepLookUpBookMenuActive, vector
 	string headingString;
 	string searchString;
 	string menuString;
-//	string lowerSearchString;
 	vector <string> titles;
+	vector <string> isbns;
 	string titleEmpty;
-	int indexBookInfo;
+	string isbnEmpty;
+	int bookIndex;
 	int booksFound = 0;
 
 	headingString = OutputClassHeading();
@@ -39,116 +40,86 @@ void lookUpBook (bool &keepInvMenuActive, bool &keepLookUpBookMenuActive, vector
 	cout << headingString;
 	cout << menuString;
 
-		for (unsigned int i = 0; i < bookType::bookCount; i++) //Creates a list of lowercase titles
-		{
-			for (int j = 0; j < int(books.at(i).getTitle().length()); j++)
-			{
-				titleEmpty.push_back(char(tolower(books.at(i).getTitle()[j])));
-			}
-			titles.push_back(titleEmpty);
-			titleEmpty.clear();
-		}
-			cin.clear();
-
-	do
+	for (unsigned int i = 0; i < bookType::bookCount; i++) //Creates a list of lowercase titles
 	{
-		//cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "\x1b[20;35H";
-		getline(cin, searchString);
-		cout << "\x1b[27;0H";
+		for (int j = 0; j < int(books.at(i).getTitle().length()); j++)
+		{
+			titleEmpty.push_back(char(tolower(books.at(i).getTitle()[j])));
+		}
+		titles.push_back(titleEmpty);
+		titleEmpty.clear();
+	}
+	for (unsigned int i = 0; i < bookType::bookCount; i++) //Creates a list of lowercase isbns
+	{
+		for (int j = 0; j < int(books.at(i).getIsbn().length()); j++)
+		{
+			isbnEmpty.push_back(char(tolower(books.at(i).getIsbn()[j])));
+		}
+		isbns.push_back(isbnEmpty);
+		isbnEmpty.clear();
+	}
+	cin.clear();
 
+	cout << "\x1b[20;35H";
+	getline(cin, searchString);
+	cout << "\x1b[27;0H";
+
+
+	if (searchString.empty())
+		return;
+	else
+	{
 		for (char& c : searchString)
     		c = std::tolower(c);
-
-		if (!searchString.empty())
+		for (unsigned int i = 0; i < bookType::bookCount; i++)
 		{
-			for (unsigned int i = 0; i < bookType::bookCount; i++)
+			if (titles.at(i).find(searchString) != std::string::npos || isbns.at(i) == searchString)
 			{
-				if (titles.at(i).find(searchString) != std::string::npos || books.at(i).getIsbn() == searchString)
-				{
-					cout << " <" << i << "> " << books.at(i).getTitle() << endl
-						  << "ISBN: " << books.at(i).getIsbn() << endl;
-					booksFound++;
-					cout << endl;
-				}
-				if (booksFound == 0)
-				{
-					cout << "No matches found!" << endl
-						<< "Press enter to return to menu";
-					cin.get();
-					return;
-				}
+				booksFound++;
+				cout << " <" << i << "> " << books.at(i).getTitle() << endl
+					  << "     ISBN: " << books.at(i).getIsbn() << endl;
+				cout << endl;
 			}
 		}
-
-
-
-/*		if (!searchString.empty() && (searchString[0] == '0' || searchString[0] == '1'))
+		if (booksFound == 0)
 		{
-			for (int i = 0; i < int(bookType::bookCount); i++)
-			{
-				if (books.at(i).getIsbn() == searchString)
-					cout << "<" << i + 1 << "> " << books.at(i).getTitle() << endl << endl;
-			}
+			cout << "No matches found!" << endl
+				<< "Press enter to return to menu";
+			cin.get();
+			return;
 		}
-		 if(!searchString.empty())
-		{
-			for (int i = 0; i < int(searchString.length()); i++)
-			{
-				lowerSearchString.push_back(char(tolower(searchString[i])));
-			}
-
-			for (int i = 0; i < int(bookType::bookCount); i++)
-			{
-				if (titles.at(i).find(lowerSearchString) != string::npos)
-				{
-					cout << " <" << i + 1 << "> " << books.at(i).getTitle()
-                                             // << " - " 
-														   //<< books.at(i).getAuthor() << " - " 
-														//	<< books.at(i).getIsbn() << " - " << "Qty: "
-														//	<< books.at(i).getQty()
-														//	<< books.at(i).getRetail() 
-																							<< endl;
-				}
-			}
-		}
-
-		lowerSearchString.clear();
-
-*/
-
-	if(!searchString.empty())
-	{
-		do {
-		cout << endl << endl << "Enter index for book info or -1 to exit: ";
-		cin  >> indexBookInfo;
-		if(indexBookInfo == -1)
-		{
-			keepLookUpBookMenuActive = false;
-		}
-		else
-		{
-			int j = indexBookInfo - 1;
-			cout << endl;
-			cout << "Title:      "  << books[j].getTitle() << endl;
-			cout << "ISBN:       "  << books[j].getIsbn() << endl;
-			cout << "Author:     "  << books[j].getAuthor() << endl;
-			cout << "Publisher:  "  << books[j].getPublisher() << endl;
-			cout << "Date Added: "  << books[j].getDate() << endl;
-			cout << "Qty:        "  << books[j].getQty() << endl;
-			cout << "Wholesale:  "  << books[j].getWholesale() << endl;
-			cout << "Retail:     "  << books[j].getRetail() << endl;
-
-		}
-		} while (indexBookInfo != -1);
 	}
 
-	if(searchString.empty())
+
+	cout << endl << endl << "Enter index for book info or -1 to exit: ";
+	cin  >> bookIndex;
+	while (cin.fail() || (bookIndex < -1 || (bookIndex >= 0 && bookIndex >= static_cast<int>(bookType::bookCount))))
 	{
-		keepLookUpBookMenuActive = false;
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cout << "Invalid Input. Please enter a valid index for book info or -1 to exit: ";
+		cin >> bookIndex;
+	}
+	if (bookIndex == -1)
+		return;
+	else
+	{
+		cout << CLEAR_SCREEN;
+		cout << "Title:      "  << books.at(bookIndex).getTitle() << endl;
+		cout << "ISBN:       "  << books.at(bookIndex).getIsbn() << endl;
+		cout << "Author:     "  << books.at(bookIndex).getAuthor() << endl;
+		cout << "Publisher:  "  << books.at(bookIndex).getPublisher() << endl;
+		cout << "Date Added: "  << books.at(bookIndex).getDate() << endl;
+		cout << "Qty:        "  << books.at(bookIndex).getQty() << endl;
+		cout << "Wholesale:  "  << books.at(bookIndex).getWholesale() << endl;
+		cout << "Retail:     "  << books.at(bookIndex).getRetail() << endl;
+
+		cout << "\nPress enter to return";
+		cin.ignore(1000, '\n');
+		cin.get();
+		return;
 	}
 
-	} while(keepLookUpBookMenuActive);
 }
 
 //No time left due to integration issues with addBook
