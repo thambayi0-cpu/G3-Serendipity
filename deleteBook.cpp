abb837daddc5c46e86f344dfc32a1094bc082879
Thokozile Hambayi <thambayi0@saddleback.edu>
2025-11-28 14:59:49 -0800

    Added and implemented deleteBook function

--- deleteBook.cpp
+++ deleteBook.cpp
@@ -11,9 +11,69 @@
 ****************************************************************************/
 
 #include "format.h"
+#include "invMenu.h"
 
 void deleteBook (vector<bookType>& books)
 {
-	
+	bool notDone = true;
+	char choice;
+	int index;
+
+	index = lookUpBook(books);
+
+	do
+	{
+		while (index == -1)
+		{
+			cout << "\033[H\033[2J";
+			cout << "Delete another book? (Y/N) ";
+			cin.get(choice);
+			choice = toupper(choice);
+
+			while (choice != 'Y' && choice != 'N')
+			{
+				cout << "Invalid input. Please enter Y or N: ";
+				cin.ignore(1000, '\n');
+				cin.get(choice);
+				choice = toupper(choice);
+			}
+
+			if (choice == 'N')
+				return;
+			else if (choice == 'Y')
+			{
+				cin.ignore(1000, '\n');
+				index = lookUpBook(books);
+			}
+			cin.ignore(1000, '\n');
+		}
+
+		cout << "\nErase this book? (Y/N) ";
+
+		cin.get(choice);
+		choice = toupper(choice);
+
+		while (choice != 'Y' && choice != 'N')
+		{
+			cout << "Invalid input. Please enter Y or N: ";
+			cin.ignore(1000, '\n');
+			cin.get(choice);
+			choice = toupper(choice);
+		}
+
+		if (choice == 'Y')
+		{
+			books.erase(books.begin()+index);
+			bookType::bookCount--;
+			books.resize(20); //Resize the vector to avoid an out of range check
+			cout << "BOOK SUCCESSFULLY DELETED" << endl;
+		}
+
+		index = -1;
+		cin.ignore(1000, '\n');
+
+	} while (notDone);
+
+	return;
 }
 
